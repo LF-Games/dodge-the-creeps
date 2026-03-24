ac1b3fac723801d21e2afcc105ade8319651c89b
MGama <matheusgama6@hotmail.com>
2026-03-24 15:31:28 -0300

    feat: change the font size of ScoreLabel according to the score.
    dynamic growing of scorelabel's font based on score.
    
    proportion: 2x1
    max size: 64
    text centralized at scorelabel

--- hud.gd
+++ hud.gd
@@ -16,10 +16,14 @@ func show_game_over():
 	await get_tree().create_timer(1).timeout
 	$StartButton.show()
 
+var min_font_size = 24
+var max_font_size = 64
 
 func update_score(score):
 	$ScoreLabel.text = str(score)
-
+	var new_size = min_font_size + (score * 2)
+	new_size = min(new_size, max_font_size)
+	$ScoreLabel.label_settings.font_size = new_size
 
 func _on_StartButton_pressed():
 	$StartButton.hide()
